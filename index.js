fb0dd4046114cd0a60ed2adde82747a30818109a
vinicius Fernandes <vinicim.fernandez1@gmail.com>
2020-12-24 15:37:56 -0300

    palindromo

---
+++ index.js
@@ -0,0 +1,8 @@
+// Dada uma string, determine se é um palíndromo, ignorando os espaços em branco e maiúsculas/minúsculas.
+const {palindromo} = require('./palindromo');
+
+const array = 'O romano acata amores a damas amadas e Roma ataca o namoro';
+
+const resposta = palindromo(array);
+
+console.log(resposta);
---
+++ palindromo.js
@@ -0,0 +1,35 @@
+   // Uma string vazia é PALÍNDROMO.
+
+   // Um espaço em branco é PALÍNDROMO.
+
+   // Uma string com 1 caractere é PALÍNDROMO.
+   // a: true
+
+// saias: true
+
+// s a i As: true
+
+// socorram me subi no onibus em marrocos: true
+
+// O romano acata amores a damas amadas e Roma ataca o namoro: true
+
+// 23gg32: true
+
+// blablabla: false
+
+// saias!:false (devido ao ponto de exclamação)
+
+// Salve o tricolor paulista: false
+exports.palindromo = (array)=>{
+  //var re = /[\W_]/g;  
+  let arrayInicial = array.toLowerCase().split('').join('').replace(/\s/g, '');
+  console.log(arrayInicial);
+  let arrayInvertido = arrayInicial.split('').reverse().join(''); 
+  console.log(arrayInvertido)
+
+  if(arrayInvertido === arrayInicial)
+    return 'É um palindromo';
+
+    return 'Não é um palindromo';
+
+}
